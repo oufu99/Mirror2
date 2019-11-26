8941cb863347e4bd8adec9c25120f79f73d993f6
oujialin <oujialin@dossen.com>
2019-11-26 10:05:45 +0800

    批量提交

--- UpdateAllGit/UpdateAllGit/Program.cs
+++ UpdateAllGit/UpdateAllGit/Program.cs
@@ -16,25 +16,31 @@ namespace UpdateAllGit
 
             var path = ConfigHelper.GetAppConfig("GitPath");
             var pathList = path.Split(new string[] { @";" }, StringSplitOptions.RemoveEmptyEntries);
+            var taskList = new List<Task>();
             foreach (var item in pathList)
             {
-                string disk = FileHelper.GetDiskNameByFullPath(item);
-                List<string> list = new List<string>();
-                list.Add(disk);
-                list.Add($"cd {item}");
+                var task = Task.Run(() =>
+                {
+                    Console.WriteLine(item + "开始");
+                    string disk = FileHelper.GetDiskNameByFullPath(item);
+                    List<string> list = new List<string>();
+                    list.Add(disk);
+                    list.Add($"cd {item}");
 
-                list.Add("git pull");
-                list.Add("git add *");
-                list.Add("git  commit -m \"批量提交\"");
-                list.Add("git push");
+                    list.Add("git pull");
+                    list.Add("git add *");
+                    list.Add("git  commit -m \"批量提交\"");
+                    list.Add("git push");
 
-
-                string res = CMDHelper.Excute(list);
-                Console.WriteLine(res);
+                    string res = CMDHelper.Excute(list);
+                    Console.WriteLine(res);
+                });
+                taskList.Add(task);
             }
+            Task.WaitAll(taskList.ToArray());
             for (int i = 0; i < 10; i++)
             {
-               Console.WriteLine("执行完毕");
+                Console.WriteLine("执行完毕");
             }
             Console.ReadLine();
         }
