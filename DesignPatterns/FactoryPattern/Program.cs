598c7cd91a0a64d951506a965f1c8a552fd3e836
oujialin <oujialin@dossen.com>
2019-12-25 17:49:08 +0800

    aaron commit

---
+++ DesignPatterns/FactoryPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //简单工厂
+
+
+
+
+            //工厂
+
+
+
+            //抽象工厂
+
+
+        }
+    }
+
+
+    public class Operation
+    {
+        public virtual decimal GetResult()
+        {
+            throw new Exception("子类没有实现");
+
+        }
+    }
+
+
+    public class OperationFactory
+    {
+        public static Operation createOperate(string operate)//静态方法
+        {
+            Operation oper = null;
+            switch (operate)      //分支判断，在增加新的运算类的时候这里需要修改，违背了开放封闭原则
+            {
+                case "+":
+                    oper = new OperationAdd();
+                    break;
+                case "-":
+                    oper = new OperationSub();
+                    break;
+                case "*":
+                    oper = new OperationMul();
+                    break;
+                case "/":
+                    oper = new OperationDiv();
+                    break;
+            }
+            return oper;
+        }
+    }
+
+}
