902914609812f93982d8d6fb8631415ccb4b3fa0
打野的小妞 <oufu99@gmail.com>
2020-01-06 07:49:32 +0800

    aaron commit

---
+++ DesignPatterns/PubSubPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using Aaron.DataCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubSubPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RedisHelper redis = new RedisHelper();
+            var client=redis.GetClient();
+            client.Publish();
+        }
+    }
+}
