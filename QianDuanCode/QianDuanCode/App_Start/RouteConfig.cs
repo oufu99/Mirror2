66059285a8f623ca34d73bdbd2a4912ced74bf1a
打野的小妞 <oufu99@gmail.com>
2020-02-03 16:37:25 +0800

    aaron commit

--- QianDuanCode/QianDuanCode/App_Start/RouteConfig.cs
+++ QianDuanCode/QianDuanCode/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace QianDuanCode
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Tools", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
