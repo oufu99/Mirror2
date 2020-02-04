1179c79697c591714570c0fd88f2d2feda96d898
打野的小妞 <oufu99@gmail.com>
2020-02-04 11:33:32 +0800

    aa

--- QianDuanCode/QianDuanCode/Controllers/ToolsController.cs
+++ QianDuanCode/QianDuanCode/Controllers/ToolsController.cs
@@ -36,18 +36,31 @@ namespace QianDuanCode.Controllers
             var res = JsonConvert.SerializeObject(list);
             return res;
         }
-        public string GetMonitorLog(string random)
+        public string GetMonitorLog(string random, int pageIndex, int pageSize = 10)
         {
             //var url = "http://localhost:8082/api/GetMonitorLog?MonitorKey=" + random;
-            //var res = Common.PostHttpResponse(url, "");
+            //var list = Common.PostHttpResponse(url, "");
+
+            var data = new MonitorResponseModel { TotalCount = 0, Data = new List<string>() };
             var list = new List<string>() { "结果1", "结果1", "结果1", "结果1", "结果1", "结果1", "结果1", "结果1", "结果1", "结果1", "结果1" };
-            var res = JsonConvert.SerializeObject(list);
+
+
+            if (list == null)
+            {
+
+                return JsonConvert.SerializeObject(data);
+            }
+            data.TotalCount = list.Count;
+            list = list.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            data.Data = list;
+
+            var res = JsonConvert.SerializeObject(data);
             return res;
         }
 
         private string GetToken()
         {
-            var model = new RequestTokenModel()
+            var model = new
             {
                 appid = ConfigurationManager.AppSettings["appId"],
                 appkey = ConfigurationManager.AppSettings["appkey"]
@@ -60,9 +73,23 @@ namespace QianDuanCode.Controllers
 
     }
 
+
     public class RequestTokenModel
     {
         public string appid { get; set; }
         public string appkey { get; set; }
+    }
+
+    public class MonitorRequestModel
+    {
+        public string Url { get; set; }
+        public string Json { get; set; }
+        public string Random { get; set; }
+    }
+
+    public class MonitorResponseModel
+    {
+        public int TotalCount { get; set; }
+        public List<string> Data { get; set; }
     }
 }
