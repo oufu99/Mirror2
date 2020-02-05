f5bfc26b21053f47c2b389e9fee0d0695988c0c8
打野的小妞 <oufu99@gmail.com>
2020-02-05 20:47:12 +0800

    aaron commit

---
+++ RabbitMq/RabbitMqReceived/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqReceived
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channelName = "AaronMq";
+
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            Console.WriteLine("接收开始");
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                    channel.QueueDeclare(channelName, false, false, false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume(channelName, false, consumer);
+
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var msg = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("接收到了" + msg);
+                        //channel.BasicAck(ea.DeliveryTag, true);
+                    };
+                    Console.ReadLine();
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMq/RabbitMqSend/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMqSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channelName = "AaronMq";
+
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            using (var conn = factory.CreateConnection())
+            {
+                using (var channel = conn.CreateModel())
+                {
+                    channel.QueueDeclare(channelName, false, false, false, null);
+                    var prop = channel.CreateBasicProperties();
+                    prop.DeliveryMode = 2;
+                    for (int i = 0; i < 100; i++)
+                    {
+                        string msg = "遇到困难不要怕,勇敢的面对它" + i.ToString();
+                        channel.BasicPublish("", channelName, prop, Encoding.UTF8.GetBytes(msg));
+                        Console.WriteLine("发送了" + msg);
+                        Thread.Sleep(3000);
+                    }
+
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+
+}
