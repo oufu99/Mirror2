c5cbd8ff53ea30318049d1e6e87125981719c7cb
oujialin <oujialin@dossen.com>
2020-02-25 14:28:01 +0800

    aaron commit

--- LocatorModeAndMq/LocatorMode/IStudent.cs
+++ LocatorModeAndMq/LocatorMode/IStudent.cs
@@ -13,9 +13,36 @@ namespace LocatorMode
 
     public class Student : IStudent
     {
+        private IBoyStudent[] students;
+        public Student(IBoyStudent[] _boys)
+        {
+            students = _boys;
+        }
         public void Say()
         {
             Console.WriteLine("我是Student");
+        }
+    }
+
+    public interface IBoyStudent
+    {
+        void Study();
+    }
+
+    public class BoyStudent : IBoyStudent
+    {
+        public void Study()
+        {
+            Console.WriteLine("男学生开始学习");
+        }
+    }
+
+
+    public class BoyStudent1 : IBoyStudent
+    {
+        public void Study()
+        {
+            Console.WriteLine("男学生1开始学习");
         }
     }
 }
